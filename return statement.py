16e7e1c529ae2bafaf522b05d6c7a84d5a6c9c5e
nnko294 <ennko294@gmailcom.>
2025-01-23 17:51:30 +0300

    changes made to return statement.py

---
+++ return statement.py
@@ -0,0 +1,39 @@
+def add_number (a, b):
+    return a + b
+
+result = add_number (4, 8)
+print (result)
+
+def subtract_number (a, b):
+    return a - b
+
+result = subtract_number (4, 8)
+print (result)
+
+
+def multiply_number (a, b):
+    return a * b
+
+result = multiply_number (4, 8)
+print (result)
+
+def divide_number (a, b):
+    return a / b
+
+result = divide_number (4, 8)
+print (result)
+
+def modulus_number (a, b):
+    return a % b
+
+result = modulus_number (4, 8)
+print (result)
+
+multiply = lambda x, y: x * y
+
+result = multiply(4, 8)
+print(result)
+
+
+
+
